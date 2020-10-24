df1d4aaf784089972374299074edf38d1f78008c
Shivashia <42934934+Shivashia@users.noreply.github.com>
2020-10-24 23:54:10 +0530

    studentLogin

--- User-registration/student-login.php
+++ User-registration/student-login.php
@@ -1,11 +1,48 @@
 
-
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Valorant</title>
 	<meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=yes">
 	<link rel="stylesheet"  href="Templates/Homestyle.css">
+  <style type="text/css">
+    .details{
+      width: 90%;
+      margin: auto;
+      padding: 15px 15px 15px 15px;
+      border-radius: 10px;
+      border-style: solid;
+      border-width: 5px;
+      background-color: #573FA6;
+    }
+    #pydetails{
+      border-color: red;
+    }
+    /*#pydetails.video{
+      float: left;
+    }
+    #pydetails.texts{
+      float: right;
+    }*/
+    #javadetails{
+      border-color: yellow;
+    }
+    #rdetails{
+      border-color: green;
+    }
+    section {
+      border-radius: 5px;
+      padding-top: 10px;
+        padding-left: 4px;
+      font-size: 20px;
+    }
+    aside {
+      background:inherit;
+      float: right;
+      padding-left: 10px;
+    }
+    
+  </style>
 </head>
 <body>
 <nav class="navbar">
@@ -21,8 +58,7 @@
         <li><a href="http://localhost/STUDENT-COACHING-PORTAL/User-registration/aboutus.html">About</a></li>
         <!-- <li><a href="#">Services</a></li> -->
         <!-- <li><a href="http://localhost/STUDENT-COACHING-PORTAL/User-registration/register.php">Register</a></li> -->
-        <!-- <li><a href="#">Contact</a></li> -->
-        <!-- <li id="log"><a href="#">Hello, <?php echo $row["first_name"]; ?></a></li> -->
+        <li><a href="#">Hello, Shiv</a></li>
       </ul>
       <div class="icon menu-btn">
         <i class="fas fa-bars"></i>
@@ -35,7 +71,70 @@
 <div class="about">
     <div class="content">
       <div class="title">Welcome to Valorant Coaching</div>
+      <div id="pydetails" class="details">
+        <section>
+      <aside>
+        <video src="Resources/py_tutorial.mp4" controls width="320" height="180">
+          <p>If you are reading this, it is because your browser does not support the HTML5 video element.</p>
+        </video>
+      </aside>
+      <article>
+        <h2>Gameplay</h2>
+        World of Warships is a tactical, slow-paced shooter with two basic types of armament: ship guns and torpedoes. The gameplay is team-based, and allows players to work as a team. Within a team divisions can be established to allow a group of up to three players to join and fight battles together. The player's team can fight against other players (PvP) or against the AI (PvE) in three battle modes: Standard, Domination, and Epicenter. Each mode is scored on a points system.
+        <br>
+        The warships presented in the game cover periods from the early 20th century, at the dawn of dreadnought battleships, up to warships from the 1950s, including ships that were planned but never put into production. The game has four different types of ships: destroyers, cruisers, battleships, and aircraft carriers. Submarines have been added to the game as part of testing as of update 0.9.4.
+      </article>
+
+      <article>
+        <h2>Release</h2>
+        On August 16, 2011, the company website of Wargaming, developer and publisher of World of Tanks and World of Warplanes, announced World of Battleships, a free-to-play naval action MMO, intended to complete the World of war trilogy developed by the company. On August 2, 2012, the game was renamed World of Warships. After a petition opened by South Korean players was signed 40,000 times, Wargaming removed the Rising Sun Flag from Imperial Japanese warships in July 2013. After that, more than 12,000 signatures were gathered requesting reimplementation of the Rising Sun Flag, but the decision was not overturned. On November 14, 2013, the game entered closed alpha testing.
+      </article>
+    </section>
       
+      </div>
+      <br>
+      <div id="javadetails" class="details">
+        <section>
+      <aside>
+        <video src="Resources/Java_tut.mp4" controls width="320" height="180">
+          <p>If you are reading this, it is because your browser does not support the HTML5 video element.</p>
+        </video>
+      </aside>
+      <article>
+        <h2>Gameplay</h2>
+        World of Warships is a tactical, slow-paced shooter with two basic types of armament: ship guns and torpedoes. The gameplay is team-based, and allows players to work as a team. Within a team divisions can be established to allow a group of up to three players to join and fight battles together. The player's team can fight against other players (PvP) or against the AI (PvE) in three battle modes: Standard, Domination, and Epicenter. Each mode is scored on a points system.
+        <br>
+        The warships presented in the game cover periods from the early 20th century, at the dawn of dreadnought battleships, up to warships from the 1950s, including ships that were planned but never put into production. The game has four different types of ships: destroyers, cruisers, battleships, and aircraft carriers. Submarines have been added to the game as part of testing as of update 0.9.4.
+      </article>
+
+      <article>
+        <h2>Release</h2>
+        On August 16, 2011, the company website of Wargaming, developer and publisher of World of Tanks and World of Warplanes, announced World of Battleships, a free-to-play naval action MMO, intended to complete the World of war trilogy developed by the company. On August 2, 2012, the game was renamed World of Warships. After a petition opened by South Korean players was signed 40,000 times, Wargaming removed the Rising Sun Flag from Imperial Japanese warships in July 2013. After that, more than 12,000 signatures were gathered requesting reimplementation of the Rising Sun Flag, but the decision was not overturned. On November 14, 2013, the game entered closed alpha testing.
+      </article>
+    </section>
+      </div>
+      <br>
+      <div id="rdetails" class="details">
+        <section>
+      <aside>
+        <video src="Resources/R_tut.mp4" controls width="320" height="180">
+          <p>If you are reading this, it is because your browser does not support the HTML5 video element.</p>
+        </video>
+      </aside>
+      <article>
+        <h2>Gameplay</h2>
+        World of Warships is a tactical, slow-paced shooter with two basic types of armament: ship guns and torpedoes. The gameplay is team-based, and allows players to work as a team. Within a team divisions can be established to allow a group of up to three players to join and fight battles together. The player's team can fight against other players (PvP) or against the AI (PvE) in three battle modes: Standard, Domination, and Epicenter. Each mode is scored on a points system.
+        <br>
+        The warships presented in the game cover periods from the early 20th century, at the dawn of dreadnought battleships, up to warships from the 1950s, including ships that were planned but never put into production. The game has four different types of ships: destroyers, cruisers, battleships, and aircraft carriers. Submarines have been added to the game as part of testing as of update 0.9.4.
+      </article>
+
+      <article>
+        <h2>Release</h2>
+        On August 16, 2011, the company website of Wargaming, developer and publisher of World of Tanks and World of Warplanes, announced World of Battleships, a free-to-play naval action MMO, intended to complete the World of war trilogy developed by the company. On August 2, 2012, the game was renamed World of Warships. After a petition opened by South Korean players was signed 40,000 times, Wargaming removed the Rising Sun Flag from Imperial Japanese warships in July 2013. After that, more than 12,000 signatures were gathered requesting reimplementation of the Rising Sun Flag, but the decision was not overturned. On November 14, 2013, the game entered closed alpha testing.
+      </article>
+    </section>
+      </div>
+      <br>
   </div>
 </div>
 <script type="text/javascript">
@@ -56,6 +155,8 @@ cancelBtn.onclick = ()=>{
 window.onscroll = ()=>{
   this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
   }
+
+  document.getElementById("pydetails").style.display="none";
 
 
 // var slideIndex = 1;
