8286aaa7eaccc8d5268f120a0cfe0d74cd702bc2
Shivashia <42934934+Shivashia@users.noreply.github.com>
2020-10-26 23:00:23 +0530

    koi change mt karna ab

--- User-registration/register.php
+++ User-registration/register.php
@@ -25,9 +25,10 @@
       <li><a href="Homepage.html">Home</a></li>
       <li><a href="http://localhost/STUDENT-COACHING-PORTAL/User-registration/aboutus.htmln">About</a></li>
       <!-- <li><a href="#">Services</a></li> -->
-      <li><a href="http://localhost/STUDENT-COACHING-PORTAL/User-registration/register.php">Register</a></li>
+      <!-- <li><a href="http://localhost/STUDENT-COACHING-PORTAL/User-registration/register.php">Register</a></li> -->
       <!-- <li><a href="#">Contact</a></li> -->
-      <li id="log"><a href="http://localhost/STUDENT-COACHING-PORTAL/User-registration/Homepage.html">Logout</a></li>
+      <li id="log"><a href="http://localhost/STUDENT-COACHING-PORTAL/User-registration/login.php">Login</a></li>
+      <!-- <li id="log"><a href="http://localhost/STUDENT-COACHING-PORTAL/User-registration/Homepage.html">Logout</a></li> -->
     </ul>
     <div class="icon menu-btn">
       <i class="fas fa-bars"></i>
