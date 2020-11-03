107ff1291f8c33be35fcc184b22c56165ce37d89
Shivashia <42934934+Shivashia@users.noreply.github.com>
2020-11-03 20:56:49 +0530

    final edit

--- User-registration/Junk/config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	session_start();
-	// connect to database
-	$conn = mysqli_connect("localhost", "root", "NO", "valorant");
-
-	if (!$conn) {
-		die("Error connecting to database: " . mysqli_connect_error());
-	}
-?>
